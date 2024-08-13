73d695af60925539a4cbd0ebc635ba37a504b29d
YounusEjazi <100250321+YounusEjazi@users.noreply.github.com>
2024-08-14 01:05:42 +0200

    Update models.py

--- concert/models.py
+++ concert/models.py
@@ -1,21 +1,20 @@
 from datetime import datetime
-
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 
-
-# Create your models here.
-
 class Concert(models.Model):
     # concert_name
+    concert_name = models.CharField(max_length=255)
     # duration
+    duration = models.IntegerField()
     # city
+    city = models.CharField(max_length=255)
     # date
+    date = models.DateField(default=datetime.now)
 
     def __str__(self):
         return self.concert_name
-
 
 class ConcertAttending(models.Model):
     class AttendingChoices(models.TextChoices):
@@ -39,29 +38,30 @@ class ConcertAttending(models.Model):
     def __str__(self):
         return self.attending
 
-
 class Photo(models.Model):
     # id
+    id = models.IntegerField(primary_key=True)
     # pic_url
+    pic_url = models.CharField(max_length=1000)
     # event_country
+    event_country = models.CharField(max_length=255)
     # event_state
+    event_state = models.CharField(max_length=255)
     # event_city
+    event_city = models.CharField(max_length=255)
     # event_date
-
-    class Meta:
-        managed = False
+    event_date = models.DateField(default=datetime.now)
 
     def __str__(self):
         return self.pic_url
 
-
 class Song(models.Model):
     # id
+    id = models.IntegerField(primary_key=True)
     # title
+    title = models.CharField(max_length=255)
     # lyrics
-
-    class Meta:
-        managed = False
+    lyrics = models.TextField()
 
     def __str__(self):
         return self.title
